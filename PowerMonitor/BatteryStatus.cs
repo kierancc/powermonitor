f21aec32ec40041c309d645ba6335e47301b740e
kierancc <kieran.chincheong@gmail.com>
2017-07-04 16:05:54 +0200

    First checkin, tool works

---
+++ PowerMonitor/BatteryStatus.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace PowerMonitor
+{
+    public class BatteryStatus
+    {
+        private static BatteryStatus singletonBH = null;
+        private double lastVoltage, currentVoltage;
+        private uint lastCapacity, currentCapacity;
+        private int dataPointCount;
+        private BatteryStatusCode currentStatus;
+        private double voltageThreshold;
+
+        public enum BatteryStatusCode
+        {
+            OK = 0,
+            Discharging = 1,
+            LowVoltage = 2
+        }
+
+        private BatteryStatus()
+        {
+            dataPointCount = Int32.Parse(ConfigurationManager.AppSettings["dataPointCount"]);
+            lastVoltage = 0;
+            currentVoltage = 0;
+            lastCapacity = 0;
+            currentCapacity = 0;
+            currentStatus = BatteryStatusCode.OK;
+            voltageThreshold = Double.Parse(ConfigurationManager.AppSettings["voltageThreshold"]);
+        }
+
+        public static BatteryStatus Instance
+        {
+            get
+            {
+                if (singletonBH == null)
+                {
+                    singletonBH = new BatteryStatus();
+                }
+
+                return singletonBH;
+            }
+        }
+
+        public void RecordData(double voltage, uint capacity)
+        {
+            lastVoltage = currentVoltage;
+            currentVoltage = voltage;
+            lastCapacity = currentCapacity;
+            currentCapacity = capacity;
+        }
+
+        public BatteryStatusCode QueryStatus()
+        {
+            if (currentVoltage < voltageThreshold)
+            {
+                currentStatus = BatteryStatusCode.LowVoltage;
+            }
+            else if (currentCapacity < lastCapacity)
+            {
+                currentStatus = BatteryStatusCode.Discharging;
+            }
+            else
+            {
+                currentStatus = BatteryStatusCode.OK;
+            }
+
+            return currentStatus;
+        }
+    }
+}
--- PowerMonitor/MainWindow.xaml.cs
+++ PowerMonitor/MainWindow.xaml.cs
@@ -1,17 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Reflection;
+using System.Threading;
+using System.Timers;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
+using System.Windows.Forms;
+using System.Windows.Threading;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace PowerMonitor
 {
@@ -20,9 +14,127 @@ namespace PowerMonitor
     /// </summary>
     public partial class MainWindow : Window
     {
+        class CustomTimer : System.Timers.Timer
+        {
+            public MainWindow window;
+        }
+        
+        private CustomTimer monitorTimer;
+        System.Windows.Forms.NotifyIcon ni;
+
         public MainWindow()
         {
             InitializeComponent();
+            this.Closing += OnWindowClosing;
+
+            ni = new NotifyIcon();
+            ni.Icon = new System.Drawing.Icon("battery.ico");
+            ni.Visible = true;
+            ni.DoubleClick +=
+                delegate (object sender, EventArgs args)
+                {
+                    this.Show();
+                    this.WindowState = WindowState.Normal;
+                };
+        }
+
+        protected override void OnStateChanged(EventArgs e)
+        {
+            if (WindowState == WindowState.Minimized)
+                this.Hide();
+
+            base.OnStateChanged(e);
+        }
+
+        public void OnWindowClosing(object sender, EventArgs e)
+        {
+            ni.Visible = false;
+        }
+
+
+        private void btnStart_Click(object sender, RoutedEventArgs e)
+        {
+            btnStart.IsEnabled = false;
+            btnStop.IsEnabled = true;
+            monitorTimer = new CustomTimer();
+            monitorTimer.window = this;
+            monitorTimer.Interval = 1000;
+            monitorTimer.Elapsed += OnTimerElapsed;
+            monitorTimer.AutoReset = true;
+            monitorTimer.Enabled = true;
+        }
+
+        private static void OnTimerElapsed(Object src, ElapsedEventArgs e)
+        {
+            // Query battery information
+            BatteryInformation battInfo = BatteryInfo.GetBatteryInformation();
+            Object batteryChargeStatus = null;
+
+            Type t = typeof(System.Windows.Forms.PowerStatus);
+            PropertyInfo[] pi = t.GetProperties();
+            for (int i = 0; i < pi.Length; i++)
+            {
+                if (pi[i].Name == "BatteryChargeStatus")
+                {
+                    batteryChargeStatus = pi[i].GetValue(SystemInformation.PowerStatus, null);
+                    break;
+                }
+                else
+                {
+                    continue;
+                }
+            }
+
+            // Record the data
+            double voltage = battInfo.Voltage / 1000.0;
+            BatteryStatus.Instance.RecordData(voltage, battInfo.CurrentCapacity);
+            BatteryStatus.BatteryStatusCode status = BatteryStatus.Instance.QueryStatus();
+
+            if (status != BatteryStatus.BatteryStatusCode.OK)
+            {
+                Console.Beep(5000, 200);
+                Console.Beep(5000, 200);
+                Console.Beep(5000, 200);
+            }
+
+            // Update the GUI
+            MainWindow window = ((CustomTimer)src).window;
+            
+            System.Windows.Application.Current.Dispatcher.Invoke(DispatcherPriority.Background, new ThreadStart(delegate {
+                window.lblMode.Content = batteryChargeStatus.ToString();
+                window.lblVoltage.Content = String.Format("{0} V", battInfo.Voltage);
+                window.lblCapacity.Content = String.Format("{0} mWh", battInfo.CurrentCapacity);
+
+                switch(status)
+                {
+                    case BatteryStatus.BatteryStatusCode.OK:
+                        window.lblStatus.Content = "OK";
+                        window.lblStatus.Foreground = Brushes.Black;
+                        break;
+                    case BatteryStatus.BatteryStatusCode.LowVoltage:
+                        window.lblStatus.Content = "Low Voltage!";
+                        window.lblStatus.Foreground = Brushes.Red;
+                        break;
+                    case BatteryStatus.BatteryStatusCode.Discharging:
+                        window.lblStatus.Content = "Discharging!";
+                        window.lblStatus.Foreground = Brushes.Red;
+                        break;
+                }
+            }));
+        }
+
+        private void btnStop_Click(object sender, RoutedEventArgs e)
+        {
+            btnStop.IsEnabled = false;
+            btnStart.IsEnabled = true;
+            monitorTimer.Stop();
+            monitorTimer.Dispose();
+            monitorTimer = null;
+
+            lblMode.Content = "Monitoring Disabled";
+            lblVoltage.Content = "Monitoring Disabled";
+            lblCapacity.Content = "Monitoring Disabled";
+            lblStatus.Content = "";
         }
     }
 }
